90956ad5c66778f4d83225152f0a74eefdf963f0
TataruCosmin1995 <tcosmin95@yahoo.com>
2020-03-31 17:10:03 +0300

    Homework Week 7 P1

---
+++ Saptamana 7/P1/homework/index.js
@@ -0,0 +1,32 @@
+// 1.
+
+
+for (let number = 0; number < 20; number++) {
+  if (number % 2 === 0) {
+    console.log(number + ' este par');
+  }
+  else {
+    console.log(number + " este impar")
+  }
+}
+
+// 2.
+
+ let multiply = 9;
+ for (let i = 0; i <= 10; i++) {
+  let result = i * multiply;
+  console.log(multiply + ' * ' + i + ' = ' + result);
+ }
+
+// 3.
+
+let multiply = 9;
+for (let i = 0; i <= 10; i++) {
+ let result = i * multiply;
+ console.log(multiply + ' * ' + i + ' = ' + result);
+}
+
+
+4.
+
+
